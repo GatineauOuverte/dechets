48a4e99bb91b0e1fd96d4b7e14f69b2e670c7aa6
Nicolas Carriere <carriere.nic@gmail.com>
2012-09-22 15:48:46 -0400

    Start the code!

--- js/main.js
+++ js/main.js
@@ -3,5 +3,16 @@ $(document).ready(function() {
     $('#calendar').fullCalendar({
         // put your options and callbacks here
     })
-
+    
+    function getCollectionInfo(address, cb){
+        var result = traitment();
+        var result = $.get(, function());
+        cb(result);
+    }
+    
+    function fillCalendar(info){
+       var days = getCalendarFromInfo(info);
+       mycal.apiMethod1();
+    }
+    
 });
