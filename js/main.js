f5038109c9e99fa1a76851f374b07a208ee54d4e
Daniel Lauzon <daniel.lauzon@gmail.com>
2012-09-22 18:10:43 -0400

    Sections questions/reponse et visibilité

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-$(document).ready(function() {
-
-    $('#calendar').fullCalendar({
-        // put your options and callbacks here
-    })
+$(document).ready(function() {    
     
+    $('#entryform').submit(function(){
+        $("#entry").hide(); 
+        $("#result").show();
+        
+        $('#calendar').html('').fullCalendar({
+            // put your options and callbacks here
+        });
+        return false;
+    });
+    
+    $('#restart').click(function(){
+        $("#entry").show(); 
+        $("#result").hide();
+    });    
     function getCollectionInfo(address, cb){
         var result = traitment();
         var result = $.get(function(){});
