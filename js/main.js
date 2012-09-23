aa80d5138821ca8ff854779533cca5ab9981ab56
Daniel Lauzon <daniel.lauzon@gmail.com>
2012-09-23 15:49:41 -0400

    prepare for address parsing and info retreival

--- js/main.js
+++ js/main.js
@@ -1,23 +1,65 @@
 $(document).ready(function() {    
+    // wire up the submt and return buttons
+    $('#entryform').submit(submitAddress);    
+    $('#restart').click(returnToAddressEntry);
+
+    // console.log('coco');
     
-    $('#entryform').submit(function(){
-        $("#entry").hide(); 
-        $("#result").show();
-        
-        $('#calendar').html('').fullCalendar({
-            // put your options and callbacks here
+    function submitAddress(event){
+        event.preventDefault();
+        var addressInfo = $('#addressInfo').val();
+        // parse the address and lookup the info
+        getCollectionInfo(addressInfo,function(error,result){
+            console.log('addressInfo',addressInfo);
+            if (error){
+                $('#addressErrorMessage').text(error);
+                $('#addressError').show();
+            } else {
+                $("#entry").hide(); 
+                
+                $("#addressResult").text(JSON.stringify(result));
+                
+                $("#result").show();
+                
+                // remove and redraw the calendar
+                $('#calendar').html('').fullCalendar({
+                    // put your options and callbacks here
+                });
+            }
         });
-        return false;
-    });
+    }
     
-    $('#restart').click(function(){
-        $("#entry").show(); 
+    // navigation back to initial address entry screen
+    function returnToAddressEntry(){
         $("#result").hide();
-    });    
-    function getCollectionInfo(address, cb){
-        var result = traitment();
-        var result = $.get(function(){});
-        cb(result);
+        // remove the calendar, so it is ready to be redrawn on next submit
+        $('#calendar').html('');
+
+        $('#addressErrorMessage').text('');
+        $('#addressError').hide();
+        $("#entry").show(); 
+    }
+    
+    // -cette fonction parse l'addresse soumise,
+    // -va chercher l'info sur le site de la ville
+    // parse l'info retourne par lle service
+    // the callback's signature should be callback(error,result)
+    function getCollectionInfo(addressInfo, callback){
+        var rand = Math.random();
+        if (rand<.33) {
+            callback('mock parse error');
+            return;
+        }
+        
+        // call the service
+        // var result = $.get(ur,dataFromParsedAdressInfo,function(){});
+        if (rand<.66) {
+            callback('mock service error');
+            return;
+        }
+        // fake result
+        var result = {"adresse":"25 rue Laurier","secteur":"Secteur de Hull","jour":"vendredi","MC":"28-09-2012","MR":"05-10-2012","OM":"28-09-2012"}
+        callback(null,result);
     }
     
     function fillCalendar(info){
