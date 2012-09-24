b5181bf5777504cd316bb2989a6229f074c02cf0
Nicolas Carriere <carriere.nic@gmail.com>
2012-09-24 19:30:20 -0400

    Affichage des dates de collectes dans le calendrier

--- js/main.js
+++ js/main.js
@@ -82,28 +82,53 @@ $(document).ready(function() {
         var addressInfo = $('#addressInfo').val();
         // parse the address and lookup the info
         getCollectionInfo(addressInfo,function(error,result){
-            console.log('addressInfo',addressInfo);
-            if (error){
-                showError(error);
-            } else {                
-                // go to results page ....
-
-                // hide entry form
-                $("#entry").hide(); 
+            
+            //var events = new Array
+            
+            console.log(result);
+            
+            getCalendarEvents(result, function(error,events){
+                console.log("No Error");
+                console.log(events);
                 
-                // set the results
-                $("#addressResult").text(JSON.stringify(result));
-                                
-                // show the panel (before you draw the calendar!)
-                $("#result").show();
-
-                // remove and redraw the calendar
-                $('#calendar').html('').fullCalendar({
-                    // put your options and callbacks here
-                });
-                // add the calendar entries...
-                
-            }
+                if (error){
+                    showError(error);
+                    console.log("Error");
+                } else {                
+                    // go to results page ....
+                    console.log("No Error");
+                    // hide entry form
+                    $("#entry").hide(); 
+                         
+                    // set the results
+                    $("#addressResult").text(JSON.stringify(result));
+                                            
+                    // show the panel (before you draw the calendar!)
+                    $("#result").show();
+            
+                    // remove and redraw the calendar
+                    $('#calendar').html('').fullCalendar({
+                    // add the calendar entries...
+                        events: events
+                    });
+                    //    events: [
+                    //        {
+                    //            title  : 'event1',
+                    //            start  : '2012-09-28'
+                    //        },
+                    //        {
+                    //            title  : 'event2',
+                    //            start  : '2012-09-28'
+                    //        },
+                    //        {
+                    //            title  : 'event3',
+                    //            start  : '2012-09-14',
+                    //            allDay : false // will make the time show
+                    //        }
+                    //    ]
+                    //}); 
+                }
+            });
         });
     }
 
@@ -138,29 +163,96 @@ $(document).ready(function() {
             callback('Le champs addresse est requis.');
             return;
         }
-        var rand = Math.random();
-        if (rand<.33) {
-            callback('mock parse error');
-            return;
+        //var rand = Math.random();
+        //if (rand<.33) {
+        //    callback('mock parse error');
+        //    return;
+        //}
+        //
+        //// call the service
+        //// var result = $.get(ur,dataFromParsedAdressInfo,function(){});
+        //if (rand<.66) {
+        //    callback('mock service error');
+        //    return;
+        //}
+        // fake result
+        var result = {"adresse":"25 rue Laurier","secteur":"Secteur de Hull","jour":"vendredi","MC":"2012-09-28","MR":"2012-10-05","OM":"2012-09-28"}
+        callback(null,result);
+        
+        
+        //getCalendarEvents(result);
+        //return result;
+    }
+    
+    function getCalendarEvents(info, callback){
+        
+        var jour = info.jour
+        var date = info.MC
+        var events = new Array
+        
+        var i = 0
+        var collecteInfo =  new Array
+        if (info.CR){
+            collecteInfo[i] = "CR";
+        } else {
+            if (info.AN) {
+                collecteInfo[i] = "AN";
+                i ++;
+            }
+            if (info.MC) {
+                collecteInfo[i] = "MC";
+                i ++;
+                if(info.MC == info.MR){
+                    collecteInfo[i] = "MR";
+                    i ++;    
+                }else if((info.MC == info.OM)){
+                    collecteInfo[i] = "OM";
+                    i ++;    
+                }
+            }
         }
         
-        // call the service
-        // var result = $.get(ur,dataFromParsedAdressInfo,function(){});
-        if (rand<.66) {
-            callback('mock service error');
-            return;
-        }
-        // fake result
-        var result = {"adresse":"25 rue Laurier","secteur":"Secteur de Hull","jour":"vendredi","MC":"28-09-2012","MR":"05-10-2012","OM":"28-09-2012"}
-        callback(null,result);
-    }
-    
-    function fillCalendar(info){
-       var days = getCalendarFromInfo(info);
-       mycal.apiMethod1();
-    }
+        console.log(collecteInfo)
+        
+        $.getJSON('data/collecte2012.json', function (data) {
+        
+            for (var i = 0, len = data.length; i < len; i++) {
+                if (data[i].jour == jour && compareCollecteArray(collecteInfo, data[i].collecte) && data[i].date == date){
+                    var secteur = data[i].secteur;
+                }
+            }
+                       
+            for (var i = 0, len = data.length; i < len; i++) {
+                if (data[i].secteur == secteur && data[i].jour == jour){
+                    events.push({title:'Test',start:data[i].date});
+                }
+            }
+
+            callback(null, events);
+
+        });
+            }
     
 });
+
+function compareCollecteArray(searchResult, jsonResult){
+    
+    var equal = false
+    
+    if (searchResult.length == jsonResult.length){
+        for (i = 0; i < searchResult.length; i++){
+            if (searchResult[i] == jsonResult[i]){
+                equal = true;
+            } else{
+                equal = false;
+                break;
+            }
+        }
+        //console.log(equal)
+    }
+    
+    return equal
+}
 
 function parseAddress(rawAddress) {
     var parsedAddress = {
